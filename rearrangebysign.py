ce584406005be108a507bba79735bb200123990a
Kiranmai U I <kiranmaiumesh1205@gmail.com>
2026-08-29 22:29:25 +0530

    rearrangebysign

---
+++ rearrangebysign.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0]*n
+        pos = 0
+        neg = 1
+        for x in nums :
+            if x >0 :
+                ans[pos] = x
+                pos+= 2
+            else :
+                ans[neg] = x
+                neg+= 2
+        return ans 
